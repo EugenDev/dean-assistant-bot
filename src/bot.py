66c27cd1b4aa9c045b50ff426146af8c0f013249
EugenDev <moaisdean@gmail.com>
2021-02-13 23:12:36 +0500

    create tables in handler constructors

--- src/bot.py
+++ src/bot.py
@@ -11,12 +11,6 @@ from PressureReportHandler import PressureReportHandler
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, MessageFilter
         
-def create_tables(db_connection):
-    with open(r'..\create_tables.sql', 'r') as content_file:
-        init_script_lines =  content_file.readlines()
-        for line in init_script_lines:
-            db_connection.execute(line)
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger("Assistant bot")
 
@@ -30,21 +24,18 @@ config.read(path_to_settings)
 
 db_file_name = config["database"]["db_file_name"]
 db_connection = sqlite3.connect(db_file_name, check_same_thread=False)
-create_tables(db_connection)
 
 tg_apikey = config["telegram"]["api_key"]
 updater = Updater(tg_apikey, use_context=True)
 
-dispatcher = updater.dispatcher    
-
 pressure_handler = PressureHandler(db_connection, logger)
-dispatcher.add_handler(MessageHandler(pressure_handler.get_filter(), pressure_handler.handle))
+updater.dispatcher.add_handler(MessageHandler(pressure_handler.get_filter(), pressure_handler.handle))
 
 pressure_report_handler = PressureReportHandler(db_connection, logger)
-dispatcher.add_handler(MessageHandler(pressure_report_handler.get_filter(), pressure_report_handler.handle))
+updater.dispatcher.add_handler(MessageHandler(pressure_report_handler.get_filter(), pressure_report_handler.handle))
 
 error_handler = ErrorHandler(logger)
-dispatcher.add_error_handler(error_handler.handle)
+updater.dispatcher.add_error_handler(error_handler.handle)
 
 updater.start_polling()
 updater.idle()
